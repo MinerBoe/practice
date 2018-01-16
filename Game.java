b010c9166dcd985d97bb5031708cfec202132aef
Mats Dale <14minerboe16@gmail.com>
2018-01-16 11:00:56 -0700

    Refactored

--- Game.java
+++ Game.java
@@ -5,15 +5,27 @@ class Game {
 
 	public static void main(String[] args) {
 
-		Random random = new Random();
+		Random random = new Random(); // number in the parens gives the same random sequence everytime for debugging
 		Scanner scanner = new Scanner(System.in);
 
 		int number = random.nextInt(1000) + 1;
-		for (int i = 0; i < 3; i++) {
-				System.out.println("Guess a number between 1 and 1000:");
-				int guess = scanner.nextInt();
-				checker(guess, number);
+		int guesses = 0;
+
+		System.out.println("Guess a number between 1 and 1000:");
+		int guess = scanner.nextInt();
+		checker(guess, number);
+
+		while (guesses < 6 && guess != number) {
+
+			guesses++;
+
+			System.out.println("Guess a number between 1 and 1000:");
+			guess = scanner.nextInt();
+			checker(guess, number);
+
 		}
+
+		System.out.println("The number was: " + number);
 	}
 
 	public static void checker(int guess, int target) {
