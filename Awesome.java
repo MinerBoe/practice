cad4dbbf1d7b5b8914e0f2717f96994f591dd76a
Mats Dale <14minerboe16@gmail.com>
2018-01-16 10:40:19 -0700

    Refactored

--- Awesome.java
+++ Awesome.java
@@ -1,21 +1,21 @@
 class Awesome {
 
 	public static void main(String[] args) {
+
 		int times = Integer.parseInt(args[0]);
+		String message = "You're the best";
 		int exclamation = 0;
+
 		if (args.length > 1) {
 			exclamation = Integer.parseInt(args[1]);
+			for (int i = 0; i < exclamation; i++) {
+				message += "!";
+			} 
 		}
-
-		String points = "";
-		for (int i = 0; i < exclamation; i++) {
-			points = points + "!";
-		} 
-		
 
 		for (int i=0; i < times; i++) {
 
-			System.out.println("You're the best" + points);
+			System.out.println(message);
 		}
 	}
 }
