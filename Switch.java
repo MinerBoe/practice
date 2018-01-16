3d49b64991dc728dc37cb5a7e225dc5f8be9b662
Mats Dale <14minerboe16@gmail.com>
2018-01-16 11:15:16 -0700

    Initial setup

---
+++ Switch.java
@@ -0,0 +1,68 @@
+class Switch {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+	public static String switcher(String initial, String swapA, String swapB) {
+
+		for () {
+
+			if (initial.substring(i, i+swapA.length()) == swapA) {
+
+			}
+		}
+		// loop through initial in segments equal to the length of swapA
+			// look for places that are the same as swapA
+			// replace the current segment with a temporary swap string
+
+		// do everything for swapB and replace with swapA
+
+		// look for the temporary string and replace with swapB
+
+	}
+
+
+
+
+
+
+	// String statement = args[0];
+	// 	int length = statement.length();
+	// 	String aSubstring = args[1];
+	// 	String bSubstring = args[2];
+
+	// 	if (aSubstring.length() > 1) {
+	// 		aSubstring = aSubstring
+	// 	} else {
+	// 		char a = aSubstring.charAt(0);
+	// 	}
+	// 	// for (char c : aSubstring) {
+	// 	// 	a = a + c;
+	// 	// }
+	// 	char b = bSubstring.charAt(0);
+	// 	// for (char c : bSubstring) {
+	// 	// 	b = b + c;
+	// 	// }
+
+	// 	String newStatement = "";
+
+	// 	for (int i = 0; i < statement.length(); i++) {
+	// 		char c = statement.charAt(i);
+	// 		if (c == a) {
+
+	// 			newStatement = newStatement + bSubstring;
+
+	// 		} else if (c == b) {
+
+	// 			newStatement = newStatement + aSubstring;
+	// 		} else {
+
+	// 			newStatement = newStatement + c;
+	// 		}
+
+	// 	}
+
+	// 	System.out.println(newStatement);
+}
